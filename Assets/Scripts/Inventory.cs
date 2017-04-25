510e3fee6fe626f34ef3085fca4fb1a7ff4de6f2
Gramnin <gramniningram@gmail.com>
2017-04-25 14:18:04 -0500

    Dump
    Just syncing where I stopped working on inventory stuff

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -47,8 +47,16 @@ public class Inventory : MonoBehaviour {
             for (int x = 0; x < columns; x++)
             {
                 GameObject newSlot = (GameObject)Instantiate(slotPrefab);
-
                 RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+                newSlot.name = "Slot";
+                newSlot.transform.SetParent(this.transform.parent);
+
+                slotRect.localPosition = inventoryRect.localPosition + new Vector3(slotPaddingLeft + (x + 1) + (slotSize * x), -slotPaddingTop * (y + 1) - (slotSize * y));
+
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+
+                allSlots.Add(newSlot);
             }
         }
     }
