ff6718bb5753234d6011a34b388a5c659eae4e35
Gramnin <gramniningram@gmail.com>
2017-04-28 00:01:48 -0500

    Continued Inventory Work
    Began Item class, created HandleCanvas (adjusts for different screen
    sizes)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {HEALTH};
+
+public class Item : MonoBehaviour
+{
+    public ItemType type;
+
+    public Sprite spriteNeutral;
+    public Sprite spriteHighlighted;
+
+    public int maxSize;
+
+    public void Use()
+    {
+        switch (type)
+        {
+            case ItemType.HEALTH:
+                Debug.Log("I just used a first aid kit");
+                break;
+            default:
+                break;
+        }
+    }
+}
