fbe8c20a789aabea8687841d213cffda4a5e820f
Gramnin <gramniningram@gmail.com>
2017-04-28 09:57:48 -0500

    Added Humble Abode Splash Screen

---
+++ Assets/Scripts/LogoFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogoFade : MonoBehaviour
+{
+    public Image logo;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        logo.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    void FadeIn()
+    {
+        logo.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        logo.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
